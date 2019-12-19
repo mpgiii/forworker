f4243295ac4f1d9d7cae80263dcd899a48abd5c5
Michael Georgariou III <mpgiii@ymail.com>
2019-12-19 11:12:01 -0800

    Make all files php, move header/footer to common file

--- contact.php
+++ contact.php
@@ -1,75 +1,6 @@
-
-<?php
-	$errorset = False;
-	$mailsent = True;
-
-	$name = htmlspecialchars(stripslashes(trim($_POST['name'])));
-	$address = htmlspecialchars(stripslashes(trim($_POST['address'])));
-	$citystate = htmlspecialchars(stripslashes(trim($_POST['citystate'])));
-	$phone = htmlspecialchars(stripslashes(trim($_POST['phone'])));
-	$email = htmlspecialchars(stripslashes(trim($_POST['email'])));
-	$employer = htmlspecialchars(stripslashes(trim($_POST['employer'])));
-	$doi = htmlspecialchars(stripslashes(trim($_POST['doi'])));
-	$injury = htmlspecialchars(stripslashes(trim($_POST['injury'])));
-	if(!preg_match("/^[A-Za-z .'-]+$/", $name)){
-	  $name_error = 'Invalid name';
-	}
-	if(!preg_match("/^[A-Za-z0-9 ._%+-]+$/", $address)){
-	  $address_error = 'Invalid address';
-	}
-	if(!preg_match("/^[A-Za-z ,.'-]+$/", $citystate)){
-	  $citystate_error = 'Invalid City/State';
-	}
-	if(!preg_match("/^[0-9._() +-]+$/", $phone)){
-	  $phone_error = 'Invalid phone number';
-	}
-	if(!preg_match("/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,10}$/", $email)){
-	  $email_error = 'Invalid email';
-	}
-	if(!preg_match("/^[A-Za-z0-9 .,_!@#()&%+-]+$/", $employer)){
-	  $rest = preg_replace("/^[A-Za-z0-9 .,_!@#()&%+-]+$/", '-', $employer);
-	  $employer_error = 'Invalid employer';
-	}
-	if(!preg_match("/^[A-Za-z0-9 .'-\/]+$/", $doi)){
-	  $doi_error = 'Invalid date of injury';
-	}
-	if(strlen($injury) === 0){
-	  $injury_error = 'Your message should not be empty';
-	}
-	
-	if (!isset($name_error) && !isset($address_error) && !isset($citystate_error) && !isset($phone_error) && !isset($email_error) && !isset($employer_error) && !isset($doi_error) && !isset($injury_error)) {
-		$to = 'georgariou3@gmail.com'; // edit here
-		$subject = "Contact Form From: $name";
-	    $body = " Name: $name\n Address: $address, $citystate\n Phone number: $phone\n Email: $email\n Employer: $employer\n Date of injury: $doi\n Message:\n $injury";
-	    if (!@mail($to, $subject, $body)){
-			$errorset = True;
-			$mailsent = False;
-		}
-	}
-	else{
-		$errorset = True;
-	}
-?>
-
 <?php include 'header.html';?>
-<?php if(!$errorset) : ?>
     
-    <section class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_2.jpg');" data-stellar-background-ratio="0.5">
-      <div class="overlay"></div>
-      <div class="container">
-        <div class="row no-gutters slider-text align-items-end justify-content-center">
-          <div class="col-md-9 ftco-animate pb-5 text-center">
-            <h1 class="mb-3 bread">Successfully sent. Thank you for contacting us.</h1>
-            <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>Contact Us <i class="ion-ios-arrow-forward"></i></span></p>
-          </div>
-        </div>
-      </div>
-    </section>
-
-
-<?php else : ?>
-    
-    <section class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_2.jpg');" data-stellar-background-ratio="0.5">
+    <section class="hero-wrap hero-wrap-2" style="background-image: url('images/staff_wide.jpg');" data-stellar-background-ratio="0.5">
       <div class="overlay"></div>
       <div class="container">
         <div class="row no-gutters slider-text align-items-end justify-content-center">
@@ -104,58 +35,29 @@
         </div>
         <div class="row block-9">
           <div class="col-lg-8 order-md-last d-flex">
-            <form action="tempmail.php" method ="POST" class="bg-light p-5 contact-form">
-			  <?php if($mailsent) : ?>
-			    <p>There were one or more errors with your submission:</p>
-			  <?php else : ?>
-			    <p>There was an error sending your message. Please try again later.</p>
-			  <?php endif; ?>
+            <form action="mail.php" method ="POST" class="bg-light p-5 contact-form">
               <div class="form-group">
-			    <?php if(isset($name_error)) : ?>
-					<p><font color="red">Invalid name.</font></p>
-				<?php endif; ?>
                 <input type="text" class="form-control" name="name" placeholder="Name">
               </div>
               <div class="form-group">
-			    <?php if(isset($address_error)) : ?>
-					<p><font color="red">Invalid address.</font></p>
-				<?php endif; ?>
                 <input type="text" class="form-control" name="address" placeholder="Address">
               </div>
               <div class="form-group">
-				<?php if(isset($citystate_error)) : ?>
-					<p><font color="red">Invalid city/state.</font></p>
-				<?php endif; ?>
                 <input type="text" class="form-control" name="citystate" placeholder="City/State">
               </div>
 			  <div class="form-group">
-				<?php if(isset($phone_error)) : ?>
-					<p><font color="red">Invalid phone number.</font></p>
-				<?php endif; ?>
                 <input type="text" class="form-control" name="phone" placeholder="Phone number">
               </div>
 			  <div class="form-group">
-				<?php if(isset($email_error)) : ?>
-					<p><font color="red">Invalid email address.</font></p>
-				<?php endif; ?>
                 <input type="text" class="form-control" name="email" placeholder="Email">
               </div>
 			  <div class="form-group">
-				<?php if(isset($employer_error)) : ?>
-					<p><font color="red">Invalid employer name. Please do not use these characters: <?php echo $rest; ?></font></p>
-				<?php endif; ?>
                 <input type="text" class="form-control" name="employer" placeholder="Name of Employer at time of injury">
               </div>
 			  <div class="form-group">
-			    <?php if(isset($doi_error)) : ?>
-					<p><font color="red">Invalid date of injury.</font></p>
-				<?php endif; ?>
                 <input type="text" class="form-control" name="doi" placeholder="Date of injury">
               </div>
               <div class="form-group">
-			    <?php if(isset($injury_error)) : ?>
-					<p><font color="red">Invalid message.</font></p>
-				<?php endif; ?>
                 <textarea id="" cols="30" rows="7" class="form-control" name="injury" placeholder="Describe injury/conidition"></textarea>
               </div>
               <div class="form-group">
@@ -205,5 +107,4 @@
       </div>
     </section>
 
-<?php endif; ?>
 <?php include 'footer.html';?>
--- index.php
+++ index.php
@@ -1,54 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <title>Sprenkle, Georgariou & Dilles, LLP.</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    
-    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
+<?php include 'indexheader.html';?>
 
-    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
-    <link rel="stylesheet" href="css/animate.css">
-    
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="css/magnific-popup.css">
-
-    <link rel="stylesheet" href="css/aos.css">
-
-    <link rel="stylesheet" href="css/ionicons.min.css">
-
-    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
-    <link rel="stylesheet" href="css/jquery.timepicker.css">
-
-    
-    <link rel="stylesheet" href="css/flaticon.css">
-    <link rel="stylesheet" href="css/icomoon.css">
-    <link rel="stylesheet" href="css/style.css">
-  </head>
-  <body>
-    
-	  <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
-	    <div class="container">
-	      <a class="navbar-brand" href="index.html"></a>
-	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-	        <span class="oi oi-menu"></span> Menu
-	      </button>
-
-	      <div class="collapse navbar-collapse" id="ftco-nav">
-	        <ul class="navbar-nav ml-auto">
-	          <li class="nav-item active"><a href="index.html" class="nav-link">Home</a></li>
-	          <li class="nav-item"><a href="about.html" class="nav-link">About Us</a></li>
-	          <li class="nav-item"><a href="practice-areas.html" class="nav-link">Practice Areas</a></li>
-	          <li class="nav-item"><a href="attorneys.html" class="nav-link">Attorneys</a></li>
-	          <li class="nav-item"><a href="links.html" class="nav-link">Links</a></li>
-	          <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
-	        </ul>
-	      </div>
-	    </div>
-	  </nav>
-    <!-- END nav -->
     
     <div class="hero-wrap js-fullheight" style="background-image: url('images/staff.jpg');" data-stellar-background-ratio="0.5">
       <div class="overlay"></div>
@@ -59,8 +10,8 @@
             <h1 class="mb-4" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">
             	<span>Sprenkle, Georgariou & Dilles, LLP.</span>
             </h1>
-			<h2 class="subheading" data-scrollax="properties: { translateY: '30%', opacity: .2 }">Partners Michael P. Georgariou II and Nicholas J. Dilles have the reputations for quality trial skills and in-depth knowledge of California workers' compensation laws. We also provide legal services related to Social Security disability claims.<br></h2>
-            <p><a href="contact.html" class="btn btn-primary py-3 px-4">Request a Free Consultation</a></p>
+			<h2 class="subheading" data-scrollax="properties: { translateY: '30%', opacity: .2 }"><br>Partners Michael P. Georgariou II and Nicholas J. Dilles have the reputations for quality trial skills and in-depth knowledge of California workers' compensation laws. We also provide legal services related to Social Security disability claims.<br></h2>
+            <p><br><br><a href="contact.html" class="btn btn-primary py-3 px-4">Request a Free Consultation</a></p>
           </div>
         </div>
       </div>
@@ -623,98 +574,4 @@
       </div>
     </section>
 
-    <footer class="ftco-footer ftco-bg-dark ftco-section">
-      <div class="container">
-        <div class="row mb-5">
-          <div class="col-md">
-            <div class="ftco-footer-widget mb-4">
-              <h2 class="ftco-heading-2">Sprenkle, Georgariou & Dilles, LLP.</h2>
-              <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-              <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
-                <li class="ftco-animate"><a href="https://www.facebook.com/ForWorker/"><span class="icon-facebook"></span></a></li>
-              </ul>
-            </div>
-          </div>
-          <div class="col-md">
-            <div class="ftco-footer-widget mb-4 ml-md-5">
-              <h2 class="ftco-heading-2">Practice Areas</h2>
-              <ul class="list-unstyled">
-                <li><a href="#" class="py-1 d-block"><span class="ion-ios-arrow-forward mr-3"></span>Family Law</a></li>
-                <li><a href="#" class="py-1 d-block"><span class="ion-ios-arrow-forward mr-3"></span>Business Law</a></li>
-                <li><a href="#" class="py-1 d-block"><span class="ion-ios-arrow-forward mr-3"></span>Insurance Law</a></li>
-                <li><a href="#" class="py-1 d-block"><span class="ion-ios-arrow-forward mr-3"></span>Criminal Law</a></li>
-                <li><a href="#" class="py-1 d-block"><span class="ion-ios-arrow-forward mr-3"></span>Drug Offenses</a></li>
-                <li><a href="#" class="py-1 d-block"><span class="ion-ios-arrow-forward mr-3"></span>Fire Accident</a></li>
-                <li><a href="#" class="py-1 d-block"><span class="ion-ios-arrow-forward mr-3"></span>Employment Law</a></li>
-                <li><a href="#" class="py-1 d-block"><span class="ion-ios-arrow-forward mr-3"></span>Property Law</a></li>
-
-              </ul>
-            </div>
-          </div>
-          <div class="col-md">
-             <div class="ftco-footer-widget mb-4">
-              <h2 class="ftco-heading-2">Business Hours</h2>
-              <div class="opening-hours">
-              	<h4>Opening Days:</h4>
-              	<p class="pl-3">
-              		<span>Monday – Friday : 8:30am to 5:00pm</span><br>
-              		<span>Weekends : Closed</span>
-              	</p>
-              	<h4>Vacations:</h4>
-              	<p class="pl-3">
-              		<span>All Official Holidays</span>
-              	</p>
-              </div>
-            </div>
-          </div>
-          <div class="col-md">
-            <div class="ftco-footer-widget mb-4">
-            	<h2 class="ftco-heading-2">Have Any Questions?</h2>
-            	<div class="block-23 mb-3">
-	              <ul>
-	                <li><span class="icon icon-map-marker"></span><span class="text">1880 N Main St #350, Salinas, CA 93906</span></li>
-	                <li><a href="tel:(831) 449-8011"><span class="icon icon-phone"></span><span class="text">(831) 449-8011</span></a></li>
-	                <li><a href="mailto:info@forworker.com"><span class="icon icon-envelope"></span><span class="text">info@forworker.com</span></a></li>
-	              </ul>
-	            </div>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-12 text-center">
-
-            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-  Copyright &copy;<script>document.write(new Date().getFullYear());</script> Sprenkle, Georgariou & Dilles, LLP. All rights reserved.<br>
-  <font size="0">Website made with <a href="https://colorlib.com" target="_blank">Colorlib</a></font>
-  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-          </div>
-        </div>
-      </div>
-    </footer>
-    
-  
-
-  <!-- loader -->
-  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
-
-
-  <script src="js/jquery.min.js"></script>
-  <script src="js/jquery-migrate-3.0.1.min.js"></script>
-  <script src="js/popper.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/jquery.easing.1.3.js"></script>
-  <script src="js/jquery.waypoints.min.js"></script>
-  <script src="js/jquery.stellar.min.js"></script>
-  <script src="js/owl.carousel.min.js"></script>
-  <script src="js/jquery.magnific-popup.min.js"></script>
-  <script src="js/aos.js"></script>
-  <script src="js/jquery.animateNumber.min.js"></script>
-  <script src="js/bootstrap-datepicker.js"></script>
-  <script src="js/jquery.timepicker.min.js"></script>
-  <script src="js/scrollax.min.js"></script>
-  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
-  <script src="js/google-map.js"></script>
-  <script src="js/main.js"></script>
-    
-  </body>
-</html>
+<?php include 'footer.html';?>
