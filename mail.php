0584938b6c2fa1f6f29bda961465dfd6e8d55a4f
Michael Georgariou III <mpgiii@ymail.com>
2019-12-23 09:25:01 -0800

    Update background image on mail page

--- mail.php
+++ mail.php
@@ -54,7 +54,7 @@
 <?php include 'header.html';?>
 <?php if(!$errorset) : ?>
     
-    <section class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_2.jpg');" data-stellar-background-ratio="0.5">
+    <section class="hero-wrap hero-wrap-2" style="background-image: url('images/staff_wide.jpg');" data-stellar-background-ratio="0.5">
       <div class="overlay"></div>
       <div class="container">
         <div class="row no-gutters slider-text align-items-end justify-content-center">
@@ -69,7 +69,7 @@
 
 <?php else : ?>
     
-    <section class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_2.jpg');" data-stellar-background-ratio="0.5">
+    <section class="hero-wrap hero-wrap-2" style="background-image: url('images/staff_wide.jpg');" data-stellar-background-ratio="0.5">
       <div class="overlay"></div>
       <div class="container">
         <div class="row no-gutters slider-text align-items-end justify-content-center">
@@ -104,7 +104,7 @@
         </div>
         <div class="row block-9">
           <div class="col-lg-8 order-md-last d-flex">
-            <form action="tempmail.php" method ="POST" class="bg-light p-5 contact-form">
+            <form action="mail.php" method ="POST" class="bg-light p-5 contact-form">
 			  <?php if($mailsent) : ?>
 			    <p>There were one or more errors with your submission:</p>
 			  <?php else : ?>
