9cb8473c21c076db73c45b7031b5feaffebea01f
Michael Georgariou III <mpgiii@ymail.com>
2020-01-03 11:03:45 -0800

    Correct mistake in header

--- team.php
+++ team.php
@@ -18,7 +18,7 @@ echo $buffer;
                 <div class="col-md-9 ftco-animate pb-5 text-center">
                     <h1 class="mb-3 bread">Our Team</h1>
                     <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i
-                                        class="ion-ios-arrow-forward"></i></a></span> <span>Attorneys <i
+                                        class="ion-ios-arrow-forward"></i></a></span> <span>Our Team <i
                                     class="ion-ios-arrow-forward"></i></span></p>
                 </div>
             </div>
